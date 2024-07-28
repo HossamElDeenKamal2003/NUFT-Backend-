0c0c0e2f9b6930dd31a81e76eb7e620f17599b22
HossamElDeenKamal2003 <boshtahoma@gmail.com>
2024-07-29 00:59:47 +0300

    first

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-require('dotenv').config();
-
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const enforce = require('express-sslify');
 
 const authRoutes = require('./router/userRouter');
 const international = require('./router/internationalRouter');
@@ -15,23 +13,18 @@ const news = require('./router/newsRouter');
 const admission = require('./router/admission');
 const contact = require('./router/contactRouter');
 
-const app = express();
-
-// Redirect HTTP to HTTPS
-if (process.env.NODE_ENV === 'production') {
-    app.use(enforce.HTTPS({ trustProtoHeader: true }));
-}
-
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {})
+mongoose.connect('mongodb+srv://hossam2003:hossam2003@cluster0.rwl6z1n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {})
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
 // Middleware
 app.use(bodyParser.json());
-
+app.get('/ab',()=>{
+    console.log("hello")
+})
 // Routes
 app.use('/auth', authRoutes);
 app.use('/faclity', international);
@@ -42,8 +35,10 @@ app.use('/news', news);
 app.use('/admission', admission);
 app.use('/contact', contact);
 
+// CORS OPTIONS preflight handler
+app.options('/auth/signup', cors());
+
 // Server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.listen(3000, () => {
+    console.log('Server Running on port 3000');
 });
